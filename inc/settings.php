d5679cf4672879250f1e415bd6f2259c271e45c8
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2016-09-12 00:34:04 +0800

    优化设置

--- inc/settings.php
+++ inc/settings.php
@@ -15,9 +15,12 @@ function wizhi_enter_settings() {
 			'name'     => 'wizhi_enter_settings',
 			'tabbed'   => 'horizontal',
 			'serialize_data' => false,
+			'add_to_prefix'  => false,
 			'children' => [
 				'site' => new Fieldmanager_Group( [
 					'label'    => __( 'Site Information', 'enter' ),
+					'serialize_data' => false,
+					'add_to_prefix'  => false,
 					'children' => [
 						"favicon" => new Fieldmanager_Media( __( 'Favicon', 'enter' ) ),
 						"logo"    => new Fieldmanager_Media( __( 'Site Logo', 'enter' ) ),
@@ -26,6 +29,8 @@ function wizhi_enter_settings() {
 				] ),
 				'code' => new Fieldmanager_Group( [
 					'label'    => __( 'Code Settings', 'enter' ),
+					'serialize_data' => false,
+					'add_to_prefix'  => false,
 					'children' => [
 						"before_head" => new Fieldmanager_TextArea( __( 'Code before </head>', 'enter' ) ),
 						"before_body" => new Fieldmanager_TextArea( __( 'Code before </body>', 'enter' ) ),
