2f77c117cbda7d012ca55b5031bc0021d1fe86ef
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2016-10-12 11:24:51 +0800

    分类方法模板没有设置的情况下， 默认使用文章类型的模板

--- taxonomy.php
+++ taxonomy.php
@@ -1,12 +1,21 @@
 <?php
 #获取分类模板设置
-$term_id        = get_queried_object_id();
-$template       = get_term_meta( $term_id, '_term_template', true );
-$queried_object = get_queried_object();
+$object   = get_queried_object();
+$term_id  = get_queried_object_id();
+$template = get_term_meta( $term_id, '_term_template', true );
+
+if ( function_exists( 'get_post_types_by_taxonomy' ) ) {
+    $post_type          = get_post_types_by_taxonomy( $object->taxonomy );
+    $post_type_settings = get_archive_option( $post_type[ 0 ] );
+
+    if ( $post_type_settings & ! $template ) {
+        $template = $post_type_settings[ 'template' ];
+    }
+}
 
 #如果分类中设置了模板，调用模板
 if ( ! empty( $template ) ) {
-    get_template_part( 'wizhi/taxonomy/archive', $template );
+    get_template_part( 'wizhi/archive/archive', $template );
 } else {
-    get_template_part( 'wizhi/taxonomy/archive' );
+    get_template_part( 'wizhi/archive/archive' );
 }
