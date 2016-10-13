aa74bc0cbf5f79b3691e7ec832ee1404e1b8bba9
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2016-10-13 09:08:00 +0800

    升级前端工作流所用的 nvm 到4.6 lts

--- front/link.sh
+++ front/link.sh
@@ -1,5 +1,16 @@
 npm link asset-builder
+npm link browser-sync
 npm link del
+npm link imagemin-pngcrush
+npm link jshint-stylish
+npm link lazypipe
+npm link merge-stream
+npm link minimist
+npm link run-sequence
+npm link traverse
+npm link wiredep
+npm link yargs
+
 npm link gulp
 npm link gulp-autoprefixer
 npm link gulp-changed
@@ -9,32 +20,21 @@ npm link gulp-if
 npm link gulp-imagemin
 npm link gulp-jshint
 npm link gulp-less
+npm link gulp-sass
 npm link gulp-load-plugins
 npm link gulp-cssnano
-npm link gulp-minify-css
 npm link gulp-plumber
 npm link gulp-rename
 npm link gulp-rev
 npm link gulp-sourcemaps
 npm link gulp-uglify
-npm link imagemin-pngcrush
-npm link jshint-stylish
-npm link lazypipe
-npm link merge-stream
-npm link run-sequence
-npm link traverse
-npm link wiredep
-npm link yargs
-npm link minimist
-npm link browser-sync
-npm link gulp-sass
+
+npm link gulp-coffee
+npm link gulp-pxtorem
+
 npm link sprity
 npm link sprity-less
 npm link gulp-copy2
 npm link gulp-shell
 npm link gulp-gh-pages
 npm link harp
-npm link sprity
-npm link sprity-less
-npm link gulp-coffee
-npm link gulp-pxtorem
