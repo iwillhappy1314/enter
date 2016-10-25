a1ee2d05e853b435496e9f76d293752326e4d7e6
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2016-10-25 10:55:13 +0800

    更新注释

--- functions.php
+++ functions.php
@@ -1,168 +1,134 @@
 <?php
-/**
- * enter functions and definitions.
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package enter
- */
-
-if ( ! function_exists( 'enter_setup' ) ) :
-/**
- * 设置默认选项、添加 WordPress 功能支持
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function enter_setup() {
-	/*
-	 * 主题翻译支持
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on enter, use a find and replace
-	 * to change 'enter' to the name of your theme in all the template files.
-	 */
-	load_theme_textdomain( 'enter', get_template_directory() . '/languages' );
-
-	// 添加默认的文章和评论 RSS 源链接到 head 中
-	add_theme_support( 'automatic-feed-links' );
-
-	/*
-	 * 添加标题标签支持
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
-
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
+	/**
+	 * 主题功能和定义
 	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 * @package enter
 	 */
-	add_theme_support( 'post-thumbnails' );
-	add_image_size( 'archive', 200, 150, true );
-	add_image_size( 'masonry', 210, 160, true );
 
-	// 菜单位置，在 wp_nav_menu() 中使用
-	register_nav_menus( array(
-		'primary' => esc_html__( 'Primary', 'enter' ),
-		'footer' => esc_html__( 'Footer', 'enter' ),
-	) );
+	if ( ! function_exists( 'enter_setup' ) ) :
+		/**
+		 * 设置默认选项、添加 WordPress 功能支持
+		 */
+		function enter_setup() {
+			/*
+			 * 主题翻译支持
+			 */
+			load_theme_textdomain( 'enter', get_template_directory() . '/languages' );
 
-	/*
-	 * 转化默认的表单元素为 HTML5
-	 */
-	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	) );
+			// 添加默认的文章和评论 RSS 源链接到 head 中
+			add_theme_support( 'automatic-feed-links' );
+
+			/*
+			 * 添加标题标签支持
+			 */
+			add_theme_support( 'title-tag' );
+
+			/*
+			 * 文章缩略图大小
+			 */
+			add_theme_support( 'post-thumbnails' );
+			add_image_size( 'archive', 200, 150, true );
+			add_image_size( 'masonry', 210, 160, true );
+
+			/*
+			 * 菜单位置，在 wp_nav_menu() 中使用
+			 */
+			register_nav_menus( [
+				'primary' => esc_html__( 'Primary', 'enter' ),
+				'footer'  => esc_html__( 'Footer', 'enter' ),
+			] );
+
+			/*
+			 * 转化默认的表单元素为 HTML5
+			 */
+			add_theme_support( 'html5', [
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+			] );
+
+			/**
+			 * 支持 WooCommerce
+			 */
+			add_theme_support( 'woocommerce' );
+
+			/**
+			 * 添加编辑器样式
+			 */
+			add_editor_style( get_template_directory_uri() . '/front/dist/styles/editor.css' );
+
+			/**
+			 * 基于主题设计和样式, 设置内容宽度
+			 */
+			function enter_content_width() {
+				$GLOBALS[ 'content_width' ] = apply_filters( 'enter_content_width', 640 );
+			}
+
+		}
+	endif;
+
+	add_action( 'after_setup_theme', 'enter_setup' );
 
 	/**
-     * 支持 WooCommerce
-     */
-	add_theme_support( 'woocommerce' );
-
-    /**
-     * 添加编辑器样式
-     */
-    add_editor_style( get_template_directory_uri() . '/front/dist/styles/editor.css' );
-
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'enter_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
-}
-endif;
-add_action( 'after_setup_theme', 'enter_setup' );
-
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function enter_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'enter_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'enter_content_width', 0 );
-
-/**
- * 注册小工具区域
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function enter_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'enter' ),
-		'id'            => 'sidebar-main',
-		'description'   => esc_html__( 'Add widgets here.', 'enter' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'enter_widgets_init' );
-
-
-/**
- * 添加样式和脚本
- */
-function wizhi_enter_scripts() {
-	wp_enqueue_style( 'enter-style', get_template_directory_uri() . '/front/dist/styles/main.css' );
-	wp_enqueue_script( 'enter-script', get_template_directory_uri() . '/front/dist/scripts/main.js', [ 'jquery' ], '20151215', true );
-
-	wp_enqueue_style( 'enter-style-old', get_template_directory_uri() . '/front/dist/styles/old.css' );
-	wp_style_add_data( 'enter-style-old', 'conditional', 'lt IE 9' );
-
-    wp_enqueue_script('enter-html5shiv', get_template_directory_uri() . '/front/dist/scripts/html5shiv.js', [], '30', false);
-    wp_script_add_data('enter-html5shiv', 'conditional', 'lt IE 9');
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	 * 注册小工具区域
+	 */
+	function enter_widgets_init() {
+		register_sidebar( [
+			'name'          => esc_html__( 'Sidebar', 'enter' ),
+			'id'            => 'sidebar-main',
+			'description'   => esc_html__( 'Add widgets here.', 'enter' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		] );
 	}
-}
-add_action( 'wp_enqueue_scripts', 'wizhi_enter_scripts' );
+
+	add_action( 'widgets_init', 'enter_widgets_init' );
 
 
-$options = get_option('wizhi_enter_settings'); 
+	/**
+	 * 添加样式和脚本
+	 */
+	function wizhi_enter_scripts() {
+		wp_enqueue_style( 'enter-style', get_template_directory_uri() . '/front/dist/styles/main.css' );
+		wp_enqueue_script( 'enter-script', get_template_directory_uri() . '/front/dist/scripts/main.js', [ 'jquery' ], '20151215', true );
+
+		wp_enqueue_style( 'enter-style-old', get_template_directory_uri() . '/front/dist/styles/old.css' );
+		wp_style_add_data( 'enter-style-old', 'conditional', 'lt IE 9' );
+
+		wp_enqueue_script( 'enter-html5shiv', get_template_directory_uri() . '/front/dist/scripts/html5shiv.js', [], '30', false );
+		wp_script_add_data( 'enter-html5shiv', 'conditional', 'lt IE 9' );
+
+		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+			wp_enqueue_script( 'comment-reply' );
+		}
+	}
+
+	add_action( 'wp_enqueue_scripts', 'wizhi_enter_scripts' );
 
 
-/**
- * 实现自定义头部功能
- */
-require get_template_directory() . '/inc/custom-header.php';
+	$options = get_option( 'wizhi_enter_settings' );
 
-/**
- * 自定义模板标签
- */
-require get_template_directory() . '/inc/template-tags.php';
+	/**
+	 * 自定义模板标签
+	 */
+	require get_template_directory() . '/inc/template-tags.php';
 
-/**
- * 主题附加功能函数
- */
-require get_template_directory() . '/inc/extras.php';
+	/**
+	 * 主题附加功能函数
+	 */
+	require get_template_directory() . '/inc/extras.php';
 
-/**
- * 主题自定义
- */
-require get_template_directory() . '/inc/customizer.php';
+	/**
+	 * 自定义字段
+	 */
+	require get_template_directory() . '/inc/fields.php';
 
-
-/**
- * 自定义字段
- */
-require get_template_directory() . '/inc/fields.php';
-
-
-/**
- * 主题设置
- */
-require get_template_directory() . '/inc/settings.php';
+	/**
+	 * 主题设置
+	 */
+	require get_template_directory() . '/inc/settings.php';
 
--- inc/extras.php
+++ inc/extras.php
@@ -1,206 +1,165 @@
 <?php
-/**
- * Custom functions that act independently of the theme templates.
- *
- * Eventually, some of the functionality here could be replaced by core features.
- *
- * @package enter
- */
+	/**
+	 * 主题附加的功能函数
+	 *
+	 * @package enter
+	 */
 
-/**
- * 添加文章排版优化 class 到文章内容
- *
- * @param $classes
- *
- * @return array
- */
-function enter_editer_class( $classes ) {
-	$classes[] = 'typo';
+	/**
+	 * 添加文章排版优化 class 到文章内容
+	 *
+	 * @param $classes
+	 *
+	 * @return array
+	 */
+	function enter_editer_class( $classes ) {
+		$classes[] = 'typo';
 
-	return $classes;
-}
-
-add_filter( 'post_class', 'enter_editer_class' );
-
-
-/**
- * 添加 CSS 到编辑器 body
- */
-function enter_tiny_mce_before_init( $init_array ) {
-	$init_array['body_class'] = 'typo hentry entry-content';
-	return $init_array;
-}
-add_filter('tiny_mce_before_init', 'enter_tiny_mce_before_init');
-
-
-/**
- * Adds custom classes to the array of body classes.
- *
- * @param array $classes Classes for the body element.
- *
- * @return array
- */
-function enter_body_classes( $classes ) {
-	// Adds a class of group-blog to blogs with more than 1 published author.
-	if ( is_multi_author() ) {
-		$classes[] = 'group-blog';
+		return $classes;
 	}
 
-	// Adds a class of hfeed to non-singular pages.
-	if ( ! is_singular() ) {
-		$classes[] = 'hfeed';
+	add_filter( 'post_class', 'enter_editer_class' );
+
+
+	/**
+	 * 添加 CSS 到编辑器 body
+	 */
+	function enter_tiny_mce_before_init( $init_array ) {
+		$init_array[ 'body_class' ] = 'typo hentry entry-content';
+
+		return $init_array;
 	}
 
-	return $classes;
-}
-
-add_filter( 'body_class', 'enter_body_classes' );
+	add_filter( 'tiny_mce_before_init', 'enter_tiny_mce_before_init' );
 
 
-/**
- * 设置缩略图质量
- */
-add_filter( 'wp_editor_set_quality', 'wp_editor_set_quality_medium' );
-function wp_editor_set_quality_medium( $quality ) {
-	return 70;
-}
-
-
-/**
- * 修改 SSL 缩略图链接
- *
- * @param $url
- * @param $post_id
- *
- * @return string
- */
-function ssl_post_thumbnail_urls( $url, $post_id ) {
-	//Skip file attachments
-	if ( ! wp_attachment_is_image( $post_id ) ) {
-		return $url;
-	}
-
-	//Correct protocol for https connections
-	list( $protocol, $uri ) = preg_split( '@(://)@', $url, 2 );
-	if ( is_ssl() ) {
-		if ( 'http' == $protocol ) {
-			$protocol = 'https';
+	/**
+	 * 添加自定义 CSS 类到 Body 元素
+	 *
+	 * @return array
+	 */
+	function enter_body_classes( $classes ) {
+		// 多个作者时, 添加群组博客
+		if ( is_multi_author() ) {
+			$classes[] = 'group-blog';
 		}
-	} else {
-		if ( 'https' == $protocol ) {
-			$protocol = 'http';
+
+		return $classes;
+	}
+
+	add_filter( 'body_class', 'enter_body_classes' );
+
+
+	/**
+	 * 设置缩略图质量
+	 */
+	add_filter( 'wp_editor_set_quality', 'wp_editor_set_quality_medium' );
+	function wp_editor_set_quality_medium( $quality ) {
+		return 70;
+	}
+
+	/**
+	 * 预览链接新窗口打开
+	 */
+	add_action( 'admin_bar_menu', 'customize_my_wp_admin_bar', 80 );
+	function customize_my_wp_admin_bar( $wp_admin_bar ) {
+
+		// 获取节点并修改打开方式
+		$node                   = $wp_admin_bar->get_node( 'site-name' );
+		$node->meta[ 'target' ] = '_blank';
+
+		// 更新节点
+		$wp_admin_bar->add_node( $node );
+
+	}
+
+
+	/**
+	 * 修改自动摘要长度
+	 */
+	function wizhi_custom_excerpt_length( $length ) {
+		return 220;
+	}
+
+	add_filter( 'excerpt_length', 'wizhi_custom_excerpt_length', 999 );
+
+
+	/**
+	 * 后台显示缩略图标题
+	 */
+	add_filter( 'manage_posts_columns', 'wizhi_add_thumb_col' );
+	function wizhi_add_thumb_col( $cols ) {
+		$cols[ 'thumbnail' ] = __( 'Thumbnail' );
+
+		return $cols;
+	}
+
+	/**
+	 * 后台显示缩略图
+	 */
+	add_action( 'manage_posts_custom_column', 'wizhi_get_thumb_show' );
+	function wizhi_get_thumb_show( $column_name ) {
+		if ( $column_name == 'thumbnail' ) {
+			echo get_the_post_thumbnail( get_the_ID(), [ 64, 64 ] );
 		}
 	}
 
-	return $protocol . '://' . $uri;
-}
+	/**
+	 * 评论作者链接新窗口打开
+	 */
+	add_filter( 'get_comment_author_link', 'wizhi_get_comment_author_link' );
+	function wizhi_get_comment_author_link( $comment_ID ) {
+		$comment = get_comment( $comment_ID );
+		$url     = get_comment_author_url( $comment );
+		$author  = get_comment_author( $comment );
 
-add_filter( 'wp_get_attachment_url', 'ssl_post_thumbnail_urls', 10, 2 );
+		if ( empty( $url ) || 'http://' == $url ) {
+			return $author;
+		} else {
+			return '<a target="_blank" href="' . $url . '" rel="external nofollow" class="url">' . $author . '</a>';
+		}
 
-
-/**
- * 预览链接新窗口打开
- */
-add_action( 'admin_bar_menu', 'customize_my_wp_admin_bar', 80 );
-function customize_my_wp_admin_bar( $wp_admin_bar ) {
-
-	//Get a reference to the view-site node to modify.
-	$node = $wp_admin_bar->get_node( 'site-name' );
-
-	//Change target
-	$node->meta[ 'target' ] = '_blank';
-
-	//Update Node.
-	$wp_admin_bar->add_node( $node );
-
-}
-
-
-/**
- * 修改自动摘要长度
- */
-function wizhi_custom_excerpt_length( $length ) {
-	return 220;
-}
-
-add_filter( 'excerpt_length', 'wizhi_custom_excerpt_length', 999 );
-
-
-/**
- * 后台显示缩略图标题
- */
-add_filter( 'manage_posts_columns', 'wizhi_add_thumb_col' );
-function wizhi_add_thumb_col( $cols ) {
-	$cols[ 'thumbnail' ] = __( 'Thumbnail' );
-
-	return $cols;
-}
-
-/**
- * 后台显示缩略图
- */
-add_action( 'manage_posts_custom_column', 'wizhi_get_thumb_show' );
-function wizhi_get_thumb_show( $column_name ) {
-	if ( $column_name == 'thumbnail' ) {
-		echo get_the_post_thumbnail( get_the_ID(), [ 64, 64 ] );
-	}
-}
-
-/**
- * 评论作者链接新窗口打开
- */
-add_filter( 'get_comment_author_link', 'wizhi_get_comment_author_link' );
-function wizhi_get_comment_author_link( $comment_ID ) {
-	$comment = get_comment( $comment_ID );
-	$url     = get_comment_author_url( $comment );
-	$author  = get_comment_author( $comment );
-
-	if ( empty( $url ) || 'http://' == $url ) {
-		return $author;
-	} else {
-		return "<a target='_blank' href='$url' rel='external nofollow' class='url'>$author</a>";
 	}
 
-}
+	/**
+	 * 不要显示静态资源版本号
+	 */
+	add_filter( 'script_loader_src', 'wizhi_remove_script_version', 15, 1 );
+	add_filter( 'style_loader_src', 'wizhi_remove_script_version', 15, 1 );
+	function wizhi_remove_script_version( $src ) {
+		$parts = explode( '?', $src );
 
-/**
- * 不要显示静态资源版本号
- */
-add_filter( 'script_loader_src', 'wizhi_remove_script_version', 15, 1 );
-add_filter( 'style_loader_src', 'wizhi_remove_script_version', 15, 1 );
-function wizhi_remove_script_version( $src ) {
-	$parts = explode( '?', $src );
-
-	return $parts[ 0 ];
-}
+		return $parts[ 0 ];
+	}
 
 
-/**
- *  添加设置中的脚本到 wp_head()
- */
-add_action( 'wp_head', 'enter_head_script' );
-function enter_head_script() { 
-	global $options;
-	$before_head = $options['code']["before_head"];
-	echo $before_head;
-}
+	/**
+	 *  添加设置中的脚本到 wp_head()
+	 */
+	add_action( 'wp_head', 'enter_head_script' );
+	function enter_head_script() {
+		global $options;
+		$before_head = $options[ 'code' ][ "before_head" ];
+		echo $before_head;
+	}
 
-/**
- *  添加设置中的脚本到 wp_footer()
- */
-add_action( 'wp_footer', 'enter_body_script' );
-function enter_body_script() { 
-	global $options;;
-	$before_body = $options['code']['before_body'];
-	echo $before_body;
-}
+	/**
+	 *  添加设置中的脚本到 wp_footer()
+	 */
+	add_action( 'wp_footer', 'enter_body_script' );
+	function enter_body_script() {
+		global $options;;
+		$before_body = $options[ 'code' ][ 'before_body' ];
+		echo $before_body;
+	}
 
 
-// 移除存档文字
-add_filter(  'gettext',  'enter_remove_archive_text'  );
-add_filter(  'ngettext',  'enter_remove_archive_text'  );
+	// 移除存档文字
+	add_filter( 'gettext', 'enter_remove_archive_text' );
+	add_filter( 'ngettext', 'enter_remove_archive_text' );
 
-function enter_remove_archive_text( $translated ) {
-	$translated = str_ireplace( '存档：',  '',  $translated );
-	return $translated;
-}
+	function enter_remove_archive_text( $translated ) {
+		$translated = str_ireplace( '存档：', '', $translated );
+
+		return $translated;
+	}
